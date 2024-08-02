a99e4e56c8db511979da8be34d77fceb9665a5b3
lx70770 <lx70770@gmail.com>
2024-08-02 22:12:53 +0800

    feat: update ccc sdk

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,11 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
   server: {
     port: 3004,
   },
-  plugins: [react(), wasm(), topLevelAwait(), nodePolyfills()],
+  plugins: [react(), wasm(), topLevelAwait()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
 });
