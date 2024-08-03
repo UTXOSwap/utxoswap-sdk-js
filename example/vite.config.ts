b9be0e77de9d4ebb98c69065bd678a245e8b842c
lx70770 <lx70770@gmail.com>
2024-08-03 15:09:48 +0800

    fix: node polyfills

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,25 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   server: {
     port: 3004,
   },
-  plugins: [react(), wasm(), topLevelAwait()],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
+  plugins: [react(), wasm(), topLevelAwait(), nodePolyfills()],
 });
