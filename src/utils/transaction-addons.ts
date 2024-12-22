7bd90d084f7a74115bf013336ea6ba7262ab9556
lx70770 <lx70770@gmail.com>
2024-12-22 21:43:27 +0800

    fix: support usdi & rusd

--- src/utils/transaction-addons.ts
+++ src/utils/transaction-addons.ts
@@ -46,6 +46,46 @@ export const getJoyIDCellDep = (isMainnet: boolean): CKBComponents.CellDep =>
         depType: 'depGroup',
       };
 
+export const getRUSDCellDeps = (isMainnet: boolean): CKBComponents.CellDep => {
+  return isMainnet
+    ? {
+        outPoint: {
+          txHash:
+            '0x8ec1081bd03e5417bb4467e96f4cec841acdd35924538a35e7547fe320118977',
+          index: '0x0',
+        },
+        depType: 'code',
+      }
+    : {
+        outPoint: {
+          txHash:
+            '0xed7d65b9ad3d99657e37c4285d585fea8a5fcaf58165d54dacf90243f911548b',
+          index: '0x0',
+        },
+        depType: 'code',
+      };
+};
+
+export const getUSDICellDeps = (isMainnet: boolean): CKBComponents.CellDep => {
+  return isMainnet
+    ? {
+        outPoint: {
+          txHash:
+            '0xf6a5eef65101899db9709c8de1cc28f23c1bee90d857ebe176f6647ef109e20d',
+          index: '0x0',
+        },
+        depType: 'code',
+      }
+    : {
+        outPoint: {
+          txHash:
+            '0x03d029480416c2fc927dfbfe0ed1916ffaf55d1e1f3146c55cf2d3dd5e674e61',
+          index: '0x0',
+        },
+        depType: 'code',
+      };
+};
+
 export const appendCellDepsAndWitnessToUnsignedTx = (
   unsignedTx: CKBComponents.RawTransactionToSign,
   changeCapacity: bigint,
@@ -58,6 +98,8 @@ export const appendCellDepsAndWitnessToUnsignedTx = (
     getXudtDep(isMainnet),
     getCCBTCCellDeps(isMainnet),
     getJoyIDCellDep(isMainnet),
+    getRUSDCellDeps(isMainnet),
+    getUSDICellDeps(isMainnet),
   ];
 
   const witnesses: (
